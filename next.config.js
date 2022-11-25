cdff5bb5e46379fbb451073abf867a676f8a7435
zlmcdaniel <zlmcdaniel@gmail.com>
2022-11-25 15:55:49 -0500

    setup for github pages

--- next.config.js
+++ next.config.js
@@ -4,4 +4,22 @@ const nextConfig = {
   swcMinify: true,
 }
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
+
 module.exports = nextConfig
